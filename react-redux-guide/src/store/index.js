49a4f0974b70662bef8814407a0096e36748ca4f
“dongyuekai” <457545648@qq.com>
2021-04-20 16:44:42 +0800

    redux-thunk中间件使用

--- react-redux-guide/src/store/index.js
+++ react-redux-guide/src/store/index.js
@@ -3,9 +3,12 @@ import { createStore, applyMiddleware } from 'redux'
 // import reducer from './reducers/modal.reducer'
 
 import RootReducer from './reducers/root.reducer'
-import logger from './middleware/logger'
-import test from './middleware/test'
-import thunk from './middleware/thunk'
+
+// import logger from './middleware/logger'
+// import test from './middleware/test'
+// import thunk from './middleware/thunk'
+
+import thunk from 'redux-thunk'
 
 
-export const store = createStore(RootReducer, applyMiddleware(logger, test, thunk))
+export const store = createStore(RootReducer, applyMiddleware(thunk))
