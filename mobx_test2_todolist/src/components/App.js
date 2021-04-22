fe5a3b16aba255562f3a5d70a9225173a38d2fb9
“dongyuekai” <457545648@qq.com>
2021-04-22 16:10:36 +0800

    todolist

---
+++ mobx_test2_todolist/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import AddTodo from './addTodo'
+import TodoList from './todoList'
+import TodoExtra from './todoExtra'
+
+function App() {
+  return (
+    <section className='todoapp'>
+      <AddTodo />
+      <TodoList />
+      <TodoExtra />
+    </section>
+  );
+}
+
+export default App;
