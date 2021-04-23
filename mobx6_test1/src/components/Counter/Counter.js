650c8f7fa5ec845e43c5d0a0ac30d9451d3b9ccc
“dongyuekai” <457545648@qq.com>
2021-04-23 16:40:51 +0800

    reaction使用 （autorun 不同，reaction 初始时不会执行副作用）

--- mobx6_test1/src/components/Counter/Counter.js
+++ mobx6_test1/src/components/Counter/Counter.js
@@ -1,15 +1,23 @@
 import { observer } from 'mobx-react-lite'
 import { useRootStore } from '../../stores/RootStore'
-import { autorun } from 'mobx'
+import { autorun, reaction } from 'mobx'
 import { useEffect } from 'react'
 
 function Counter() {
   const { counterStore } = useRootStore()
   useEffect(() => {
-    // autorun当内部有状态更新的时候就会自动执行
-    autorun(() => {
-      console.log('dyk---', counterStore.count)
-    })
+    // autorun当内部有状态更新的时候就会自动执行 默认执行一次
+    // autorun(() => {
+    //   console.log('dyk---', counterStore.count)
+    // })
+
+    // reaction 方法提供了更加细颗粒度的状态控制 和 autorun 不同，reaction 初始时不会执行副作用
+    reaction(
+      () => counterStore.count,
+      (current, previous) => {
+        console.log('dyk---current---', current)
+        console.log('dyk---previous---', previous)
+      })
   }, [])
   return (
     <div>
